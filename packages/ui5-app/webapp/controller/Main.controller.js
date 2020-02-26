b0ed1a39cd4600f31b8568db59ffbf01baf58057
Volker Buzek <volker.buzek@js-soft.com>
2020-02-26 11:51:51 +0100

    feat(ui5-app): stable XML controls w/ data binding

--- packages/ui5-app/webapp/controller/Main.controller.js
+++ packages/ui5-app/webapp/controller/Main.controller.js
@@ -7,6 +7,19 @@ sap.ui.define(["test/Sample/controller/BaseController", "sap/ui/model/json/JSONM
                     this.getModel("LatestUI5").setProperty("/latest", latestU5version.version)
                 })
                 .catch(err => console.error(err))
+        },
+        onPeopleSelect(oEvent) {
+            this.getModel("Main").setProperty("/TabContainerBusy", true)
+            const oListItemSelected = oEvent.getParameters().listItem
+            const sPersonDetail = oListItemSelected.getBindingContext("Backend").getPath()
+            const sModelBasePath = oListItemSelected.getBindingContext("Backend").getModel().sServiceUrl // I know
+            fetch(`${sModelBasePath}${sPersonDetail}?$format=json`)
+                .then(response => response.json())
+                .then(oPersonDetailData => {
+                    this.getModel("PeopleDetail").setData(oPersonDetailData)
+                })
+                .catch(err => console.error(err))
+                .finally(this.getModel("Main").setProperty("/TabContainerBusy", false))
         }
     }
 
