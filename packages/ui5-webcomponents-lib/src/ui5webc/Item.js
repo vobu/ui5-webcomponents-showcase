387a0e154ceaa2337bbf1d4bc4d52b53411154b8
Volker Buzek <volker.buzek@js-soft.com>
2020-03-04 16:50:24 +0100

    refactor(ui5-li): syntax, jsdoc

--- packages/ui5-webcomponents-lib/src/ui5webc/Item.js
+++ packages/ui5-webcomponents-lib/src/ui5webc/Item.js
@@ -2,6 +2,7 @@ sap.ui.define(["sap/ui/core/Control"], Control => {
     const Item = Control.extend("ui5webc.Item", {
         metadata: {
             properties: {
+                // mapped to <ui5-li>$text</ui5>
                 text: { type: "string", defaultValue: "" }
             },
             events: {
@@ -11,10 +12,15 @@ sap.ui.define(["sap/ui/core/Control"], Control => {
 
         renderer: {
             apiVersion: 2,
-            render: (oRM, oLI) => {
+            /**
+             * renders as <ui5-li>some content here</ui5-li>
+             * @param {sap.ui.core.RenderManager} oRM - UI5's render manager
+             * @param {ui5webc.Item} oLI - this UI5 custom control
+             */
+            render(oRM, oLI) {
                 oRM.openStart("ui5-li", oLI)
                 oRM.openEnd()
-                oRM.text(oLI.getText())
+                oRM.text(oLI.getText()) // see metadata property "text" above
                 oRM.close("ui5-li")
             }
         }
